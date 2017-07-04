ab688ff602d478de246642670c6038c46bd7050d
Fayyaz Azam <fayyazazam@gmail.com>
2017-07-03 22:15:13 -0700

    Added formspree and validations

--- app/controllers/PageCtrl.js
+++ app/controllers/PageCtrl.js
@@ -1,20 +1,30 @@
 /**
  * Controller that handles page load effects
  */
-angular.module( 'danaApp' ).controller( 'PageCtrl',
-	function ( $scope ) {
-		console.log( "Page Controller loaded" );
+angular.module('danaApp').controller('PageCtrl',
+	function ($scope) {
+		console.log("Page Controller loaded");
 		// Animation entrance for each page
 		$scope.pageClass = 'page-effect';
-		
+
 		// Need to make sure we only send valid forms
 		// to formspree
 		$scope.submitForm = function (isValid) {
 			if (isValid) {
 				// Send the form
-				alert("sending");
-			} else {
-				alert("not sending garbage");
+				$.ajax({
+					url: "https://formspree.io/danacamillee@hotmail.com",
+					method: "POST",
+					data: {
+						name: $("#contactForm").find("input[name='name']").val(),
+						_replyto: $("#contactForm").find("input[name='email']").val(),
+						message: $("#contactForm").find("textarea[name='message']").val()
+					},
+					dataType: "json",
+					success: function () {
+						alert("success, apparently");
+					}
+				});
 			}
 		}
 	}
